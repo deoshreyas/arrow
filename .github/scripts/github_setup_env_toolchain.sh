ebb22feb5b22670efd27d2413400d47b499dfff7
deoshreyas <shreyasdeo.k50@gmail.com>
2026-03-12 15:25:38 +0530

    remove github actions

--- .github/scripts/github_setup_env_toolchain.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eux
-
-# Simple script for setting up all toolchain dependencies for building Ungoogled-Chromium on macOS
-
-brew install ninja coreutils --overwrite
-
-# Install PySocks and httplib2 for Python from PyPI
-pip3 install PySocks httplib2 --break-system-packages
